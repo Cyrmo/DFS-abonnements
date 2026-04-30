7d3a6c98f5a298d3e9bd45d30555afd5acaeff1a
Cyrille Mohr <cyrille.mohr@gmail.com>
2026-04-30 02:37:30 +0200

    fix: add lang.php to map email subjects in translation interface

---
+++ mails/fr/lang.php
@@ -0,0 +1,6 @@
+<?php
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Votre abonnement est prêt'] = 'Votre abonnement est prêt';
+$_LANGMAIL['Votre abonnement est terminé'] = 'Votre abonnement est terminé';
+$_LANGMAIL['Votre code promo abonnement'] = 'Votre code promo abonnement';
